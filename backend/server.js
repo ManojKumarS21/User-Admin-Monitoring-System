052bf31a1fb9907e816ba78be4e55c4236a35e0e
Manoj Kumar <viratmanoj8045@gmail.com>
2026-02-02 09:32:10 +0530

    Updated database config and env template

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const db = require("./db");
+const db = require("./config/db");
 
 const http = require("http");
 const WebSocket = require("ws");
